0a0e0d6ee1603de97ee9e110f50b520f575a6d2f
Matias <meneghettimatias@gmail.com>
2022-08-12 17:06:12 +0200

    .env

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 const { i18n } = require("./next-i18next.config");
-const path = require("path");
-
-const { parsed: localEnv } = require("dotenv-safe").config({
-  allowEmptyValues: false,
-  path: path.resolve(__dirname, `.env`),
-});
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n,
-  env: localEnv
+  i18n
 };
 
 module.exports = nextConfig;
